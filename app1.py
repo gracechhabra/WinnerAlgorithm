a55b0e3f1f615196feac8cd6e709f0700fcc55f8
Grace Chhabra <graechhabra@gmail.com>
2025-02-04 16:33:05 +0530

    Changed the sheet number

--- app1.py
+++ app1.py
@@ -30,7 +30,7 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_path,
 #Authorizing the file and opening the workbook
 file = gspread.authorize(credentials)
 workbook = file.open("Test")
-sheet = workbook.get_worksheet(1)
+sheet = workbook.get_worksheet(0)
 
 #Accessing the data and making a data frame
 data = sheet.get_all_records()
